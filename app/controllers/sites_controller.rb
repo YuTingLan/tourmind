4a547dafe3eb161b9274c927cc7be5c5d116717b
Betty Hsu <997666aa@gmail.com>
2023-04-21 18:56:06 +0800

    hotels and sites CRUD updated

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,40 +1,52 @@
-class SitesController < ApplicationController
-     def index
-      @sites = Site.all
-    end
-    def new 
-       @site = Site.new
-    end
-    def create
-       @site = Site.new(site_parames)
-       if @site.save
-        redirect_to sites_path, notice:'新增成功'
-       else
-        render :new
-       end
-    end
-    def show
-        @site = Site.find(params[:id])
-    end
-    def edit
-        @site = Site.find(params[:id])
-    end
-    def update
-        @site = Site.find(params[:id])
-        if  @site.update (site_parames )
-             redirect_to sites_path, notice:'更新成功'
-       else
-        render :edit
-       end
-    end
-    def destroy
-        @site = Site.find(params[:id])
-        @site.destroy
-        redirect_to sites_path, notice:'已刪除!'
-    end
+# frozen_string_literal: true
 
-    private
-    def  site_parames 
-        params.require(:site).permit(:name, :website, :adderess, :latitude, :longitude, :stay_duration, :intro, :pet_freindly)
+class SitesController < ApplicationController
+  before_action :set_site, only: %i[show edit update destroy]
+
+  def index
+    @sites = Site.order(updated_at: :desc)
+    @sites = Site.search(params[:keyword]) if params[:keyword].present?
+  end
+
+  def new
+    @site = Site.new
+  end
+
+  def create
+    @site = Site.new(site_parames)
+    if @site.save
+      redirect_to sites_path, notice: '新增成功'
+    else
+      render :new
     end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @site.update(site_parames)
+      redirect_to sites_path, notice: '更新成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_site
+    @site.destroy
+    redirect_to sites_path, notice: '已刪除!'
+  end
+
+  private
+
+  def set_site
+    @site = Site.find(params[:id])
+  end
+
+  def site_parames
+    params.require(:site).permit(:name, :website, :address, :tel, :latitude, :longitude, :stay_duration, :intro,
+                                 :pet_freindly, site_types: [])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_19_092339) do
+ActiveRecord::Schema.define(version: 2023_04_21_054819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -71,5 +71,18 @@ ActiveRecord::Schema.define(version: 2023_04_19_092339) do
     t.datetime 'updated_at', precision: 6, null: false
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "sites", force: :cascade do |t|
+    t.string "name"
+    t.string "website"
+    t.string "address"
+    t.string "latitude"
+    t.string "longitude"
+    t.float "stay_duration"
+    t.text "intro"
+    t.string "pet_freindly"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "site_types", default: [], array: true
+    t.string "tel"
   end
 end
