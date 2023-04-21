f822adceeabb5519f0d1c3b2242916a816143f78
tiger <tiger850109@gmail.com>
2023-04-21 13:42:21 +0800

    partial rendering for Show page
    rubucop
    
    redriect after create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: %i[show edit update destroy]
 
   # GET /restaurants or /restaurants.json
   def index
@@ -7,8 +7,7 @@ class RestaurantsController < ApplicationController
   end
 
   # GET /restaurants/1 or /restaurants/1.json
-  def show
-  end
+  def show; end
 
   # GET /restaurants/new
   def new
@@ -16,15 +15,14 @@ class RestaurantsController < ApplicationController
   end
 
   # GET /restaurants/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /restaurants or /restaurants.json
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.image = "https://fakeimg.pl/300x200" if @restaurant.image.empty?
+    @restaurant.image = 'https://fakeimg.pl/300x200' if @restaurant.image.empty?
     if @restaurant.save
-      redirect_to restaurant_url(@restaurant), notice: "Restaurant was successfully created." 
+      redirect_to restaurants_path, notice: 'Restaurant was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +31,7 @@ class RestaurantsController < ApplicationController
   # PATCH/PUT /restaurants/1 or /restaurants/1.json
   def update
     if @restaurant.update(restaurant_params)
-      redirect_to restaurant_url(@restaurant), notice: "Restaurant was successfully updated."
+      redirect_to restaurant_url(@restaurant), notice: 'Restaurant was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,22 +40,22 @@ class RestaurantsController < ApplicationController
   # DELETE /restaurants/1 or /restaurants/1.json
   def destroy
     @restaurant.destroy
-    redirect_to restaurants_url, notice: "Restaurant was successfully destroyed."
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :intro, :address, :lat, :long, :image, :section, :email, :tel, :website, :restaurant_type, {:cuisine_types => []}, :price,{:atmostphere => []}, :michelin_star).tap do |whitelisted|
-        whitelisted[:cuisine_types].reject!(&:empty?)
-        whitelisted[:atmostphere].reject!(&:empty?)
-      end
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-    
+  # Only allow a list of trusted parameters through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :intro, :address, :lat, :long, :image, :section, :email, :tel,
+                                       :website, :restaurant_type, { cuisine_types: [] }, :price, { atmostphere: [] }, :michelin_star).tap do |whitelisted|
+      whitelisted[:cuisine_types].reject!(&:empty?)
+      whitelisted[:atmostphere].reject!(&:empty?)
+    end
+  end
 end
