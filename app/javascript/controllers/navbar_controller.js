bab5bec8a2ef85eaffcbdb006c10a1347a899b2b
Betty Hsu <997666aa@gmail.com>
2023-04-23 09:53:22 +0800

    add comment

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,10 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
     static targets = ["navbarMenu"]
-
-    initialize() {
-    }
-
+    
+    //控制navbar小於768px的選單
     toggleNavbarMenu() {
         this.navbarMenuTarget.classList.toggle('hidden');
     }
