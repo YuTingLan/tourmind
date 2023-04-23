46cae1deee67493932c254b707ef73defd6976ce
Betty Hsu <997666aa@gmail.com>
2023-04-23 10:43:36 +0800

    update hotel_controller comment

--- app/javascript/controllers/hotel_controller.js
+++ app/javascript/controllers/hotel_controller.js
@@ -3,12 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="hotel"
 export default class extends Controller {
   static targets = ["filterMenu"]
-  //控制篩選鍵
+  //控制小於768px時出現的篩選鍵
   toggleFilterMenu() {
     this.filterMenuTarget.classList.toggle('hidden');
   }
 
-  //控制畫面左方分類Category的三角形篩選鍵
+  //控制畫面左方分類區箭頭展開鍵
   toggleFilterCategory(event) {
     const icon = event.currentTarget.children[0];
     const filterOptions = event.currentTarget.nextElementSibling;
